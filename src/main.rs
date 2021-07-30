3e81151f48fd97cb8f7eb6f1b6931f86151f1853
Michael Lowe <michael.lowe@education.govt.nz>
2021-07-30 03:28:37 +0000

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-#[macro_use]
 extern crate prettytable;
 extern crate reqwest;
 extern crate select;
 
 use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
-use prettytable::Table;
+use select::predicate::{Class};
 use std::fs;
 
 fn main() {
@@ -13,33 +11,23 @@ fn main() {
 }
 
 fn scrape_kfc(url: &str) {
-
+    println!("Scraping");
     let resp = reqwest::get(url).unwrap();
     assert!(resp.status().is_success());
 
     let document = Document::from_read(resp).unwrap();
 
-    let mut table = Table::new();
-    // let mut file_string = "Today's KFC Deals\n".to_string();
     let mut file_string = String::new();
 
     for node in document.find(Class("grid-items")) {
         let price = node.find(Class("items-price")).next().unwrap().text();
         let title = node.find(Class("items-title")).next().unwrap().text();
-        // price.what_is_it();
-        // title.what_is_it();
-        table.add_row(row![FdBybl->title]);
-        // println!("{}", file_string);
         file_string += &title;
         file_string += " | ";
         file_string += &price;
         file_string += "\n";
-        // table.add_row(row![FdBybl->price]);
-
     }
-    println!("{}",file_string);
-    // let mut file = File::create("kfc_deals.txt")?;
-    // file.write_all(file_string);
+
+    println!("{}", file_string);
     fs::write("kfc_deals.txt", file_string).expect("Unable to write file");
-    // table.printstd();
 }
